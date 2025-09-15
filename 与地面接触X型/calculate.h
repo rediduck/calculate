843c0754630e283c38ca2de8e2c85a4386b9fbe7
rediduck <39328428@qq.com>
2025-09-15 12:56:48 +0800

    增加了.h文件

--- 与地面接触X型/calculate.h
+++ 与地面接触X型/calculate.h
@@ -2,3 +2,7 @@ float getV1backcalculate(float Lx,float Ly,float Vx,float Vy,float w);
 float getV2backcalculate(float Lx,float Ly,float Vx,float Vy,float w);
 float getV3backcalculate(float Lx,float Ly,float Vx,float Vy,float w);
 float getV4backcalculate(float Lx,float Ly,float Vx,float Vy,float w);
+float getwcalculate(float Lx,float Ly,float V1,float V2,float V3,float V4);
+float getVycalculate(float Lx,float Ly,float V1,float V2,float V3,float V4);
+float getVxcalculate(float Lx,float Ly,float V1,float V2,float V3,float V4);
+
